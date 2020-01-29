5bb1d30662890ac914fa6bd9f20f3b6a6f6bdd5a
Mark Kenneth Ranosa <mark@pearlpay.io>
2020-01-29 17:40:49 +0800

    [EW-196] :construction: Wired subgenerators to badong:generate

--- generators/generate/index.js
+++ generators/generate/index.js
@@ -1,21 +1,20 @@
-// *
-//  * Copyright 2020-2020 the original author or authors from the Badong project.
-//  *
-//  * This file is part of the Badong project, see https://www.badong.tech/
-//  * for more information.
-//  *
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  *
-//  *      http://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
- 
+/**
+ * Copyright 2020-2020 the original author or authors from the Badong project.
+ *
+ * This file is part of the Badong project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const Generator = require('yeoman-generator');
 const glob = require('glob');
 
@@ -32,25 +31,23 @@ module.exports = class extends Generator {
     }
 
     writing() {
-        this.log('writing - generate');
-        this.log('generate.options.name:', this.options.name);
-
-        // this.composeWith(require.resolve('../bar'), {
-        //   options
-        // });
-        // this.composeWith(require.resolve('../baz'), {
-        //   options
-        // });
-        // TODO compose and implement me
-        // badong:model
-        // badong:use-case
-        // badong:repository
-        // badong:service
-        // badong:infra-model
-        // badong:infra-module
-        // badong:infra-repo
-        // badong:infra-view
-        // badong:infra-serializer
-        // badong:test-integration
+        let me = this;
+        let options = this.options;
+        let args = { options }
+        let subgens = [
+            '../model',
+            '../repo',
+            '../use-case',
+            '../infra-model',
+            '../infra-module',
+            '../infra-repo',
+            '../infra-view',
+            '../infra-serializer',
+            '../test-integration'
+        ];
+        
+        subgens.forEach(subgen => me.composeWith(
+            require.resolve(subgen), args)
+        );
     }
 };
